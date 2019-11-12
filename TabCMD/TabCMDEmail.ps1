d46027b5bf593ed4a1bb80fd2e889c28eedbd559
jr5050jr <57684546+jr5050jr@users.noreply.github.com>
2019-11-12 13:41:15 -0600

    Adding TabCMD Files

---
+++ TabCMD/TabCMDEmail.ps1
@@ -0,0 +1,33 @@
+$csvfile = import-csv -path "C:\Temp\regions.csv"
+
+$Folder = "C:\temp\"
+$server = "https://online.tableau.com/#/site/hosesadev694660"
+$username = "jose.cervantes@usaa.com"
+$password = "Tableau1309"
+$tabcmd = "Tabcmd"
+$URL = "/views/Regional/Obesity.pdf"
+
+
+foreach ($line in $csvfile)
+{
+  $Region = $line.Regions
+  $FileName = $Folder + $Region + ".pdf"
+  $FullURL = $URL + "?:Refresh&Region="+ $Region
+$Region
+$FileName
+$FullURL
+  & $tabcmd get -s $server -u $username -p $password  $FullURL  -f $FileName
+}
+& $tabcmd logout
+
+   $username = "tabvisualization@gmail.com"
+   $password = "Tableau1309"
+   $smtpServer = "smtp.gmail.com"
+   $to_address =  ("jcervantesSA@gmail.com","lcorral9@gmail.com")
+   $subject = "Tableau Report-" + $Region
+   $port = 587
+
+       $secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
+       $mycreds = New-Object System.Management.Automation.PSCredential ($username, $secpasswd)
+      
+ Send-MailMessage -From $username -To $to_address -SmtpServer $smtpServer -Subject $subject -Credential $mycreds -Port $port -UseSsl $true -attachment "C:\temp\West.pdf"
---
+++ TabCMD/readme.md
@@ -0,0 +1,17 @@
+install TabCMD and put the location of the folder in the path variable of the operating system so that you can run from any directory
+
+install powershell
+
+enable this in powershell so that you can execute remotely
+set-executionpolicy remotesigned
+
+using gmail to turn on less secure apps:
+https://support.google.com/accounts/answer/6010255?p=lsa_blocked&hl=en&visit_id=637091304397607969-1871269775&rd=1
+
+
+Alexa must:
+be able to call the report name, then respond back with "would you like to filter anything: then say filter by region, then say what region. 
+
+
+nice to:
+encrypt username/password for both tableau and email  - powershell can do this by running a password encryption command, then storing that in a file and passing file to a variable
